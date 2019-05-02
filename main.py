5c41ef4bc5f4469c095b1ba1f921f434cbcd9131
kaloyan <kaloyan.katsarov@gmail.com>
2019-05-02 12:16:50 +0300

    rearanging

--- main.py
+++ main.py
@@ -4,40 +4,64 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.graphics import Color
+from kivy.graphics import Rectangle
+
 
 
 class MyGrid(GridLayout):
     __criteriaCount = 1
+    __alternativeCount = 1
 
     def __init__(self, **kwargs):
         super(MyGrid, self).__init__(**kwargs)
         self.cols = 2
 
+        #self.width = 1000
+        #self.height = 1000
+
+        #with self.canvas:
+            #Color(.3, .4, .8, 0.5)
+            #self.rect = Rectangle(size=self.size,pos=self.pos)
+
         #USER INPUT GRID
-        self.userInputRoot = GridLayout()
-        self.userInputRoot.cols = 1
+        self.userInputRoot = GridLayout(size_hint_x=None, size_hint_y=None, width=500, height = 400)
+        self.userInputRoot.cols = 2
 
-        self.userCriteration = GridLayout()
+        test = Label(text="Alternative / Criteria ",size_hint_x=None, size_hint_y=None, width=150, height = 50)
+        self.userInputRoot.add_widget(test)
+
+        #USER CRITERIAS
+        self.userCriteration = GridLayout(size_hint_x=None, size_hint_y=None, width=450, height = 50)
         self.userCriteration.cols = self.__criteriaCount
+        self.userCriteration.add_widget(Label(text="Criteria " + str(self.__criteriaCount),size_hint_x=None, width = 100))
+        self.userInputRoot.add_widget(self.userCriteration) 
+ 
+        #USE ALTERNATIVES
+        self.userAlternative = GridLayout(size_hint_x=None, size_hint_y=None, width=100, height = 500)
+        self.userAlternative.cols = 1
+        self.userAlternative.add_widget(Label(text="Alternative " + str(self.__alternativeCount),size_hint_y=None, height = 50))
+        self.userInputRoot.add_widget(self.userAlternative)
 
-        self.userCriteration.add_widget(Label(text="Criteria " + str(self.__criteriaCount)))
-        
-        self.userInputRoot.add_widget(self.userCriteration)
-        
-        self.name = TextInput(multiline=False)
-        self.userInputRoot.add_widget(self.name)
+        self.userAlternativeInput = GridLayout(size_hint_x=None, size_hint_y=None, width=100, height = 500)
+        self.userAlternativeInput.cols = 1
+        self.userAlternativeInput.add_widget(TextInput(multiline=False,size_hint_y=None, height = 50))
+        self.userInputRoot.add_widget(self.userAlternativeInput)
 
+        #TextInput(multiline=False)
+
+        #ADD USER INPUT
         self.add_widget(self.userInputRoot)
 
         #CRITERATION BUTTONS
         self.criteration = GridLayout()
-        self.criteration.cols = 2
+        self.criteration.cols = 1
 
-        self.submit = Button(text="criteria +", font_size=30)
+        self.submit = Button(text="criteria +", font_size=30, size_hint_x=None, size_hint_y=None, width=150, height = 50)
         self.submit.bind(on_press=self.criteriaPlus)
         self.criteration.add_widget(self.submit)
 
-        self.submit = Button(text="criteria -", font_size=30)
+        self.submit = Button(text="criteria -", font_size=30, size_hint_x=None, size_hint_y=None, width=150, height = 50)
         self.submit.bind(on_press=self.criteriaMinus)
         self.criteration.add_widget(self.submit)
 
@@ -45,13 +69,13 @@ class MyGrid(GridLayout):
 
         #ALTERNATIVE BUTTONS
         self.alternative = GridLayout()
-        self.alternative.cols = 2
+        self.alternative.cols = 1
 
-        self.submit = Button(text="alternative +", font_size=30)
+        self.submit = Button(text="alternative +", font_size=30, size_hint_x=None, size_hint_y=None, width=170, height = 50)
         self.submit.bind(on_press=self.criteriaPlus)
         self.alternative.add_widget(self.submit)
 
-        self.submit = Button(text="alternative -", font_size=30)
+        self.submit = Button(text="alternative -", font_size=30, size_hint_x=None, size_hint_y=None, width=170, height = 50)
         self.submit.bind(on_press=self.criteriaPlus)
         self.alternative.add_widget(self.submit)
 
