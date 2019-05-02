68066e40c80457ebf7bd124eca1a6ebc1e402c5e
kaloyan <kaloyan.katsarov@gmail.com>
2019-05-02 16:33:01 +0300

    dynamically resizing

--- main.py
+++ main.py
@@ -44,9 +44,9 @@ class MyGrid(GridLayout):
         self.userInputRoot.add_widget(self.userAlternative)
 
         #USE ALTERNATIVES INPUT
-        self.userAlternativeInput = GridLayout(size_hint_x=None, size_hint_y=None, width=100, height = 500)
+        self.userAlternativeInput = GridLayout(size_hint_x=None, size_hint_y=None, width=450, height = 500)
         self.userAlternativeInput.cols = 1
-        self.userAlternativeInput.add_widget(TextInput(multiline=False,size_hint_y=None, height = 50))
+        self.userAlternativeInput.add_widget(TextInput(multiline=False,size_hint_y=None, height = 50,size_hint_x=None, width = 100))
         self.userInputRoot.add_widget(self.userAlternativeInput)
 
         #TextInput(multiline=False)
@@ -72,11 +72,11 @@ class MyGrid(GridLayout):
         self.alternative = GridLayout()
         self.alternative.cols = 1
 
-        self.submit = Button(text="alternative +", font_size=30, size_hint_x=None, size_hint_y=None, width=170, height = 50)
+        self.submit = Button(text="alternative +", font_size=25, size_hint_x=None, size_hint_y=None, width=150, height = 50)
         self.submit.bind(on_press=self.alternativePlus)
         self.alternative.add_widget(self.submit)
 
-        self.submit = Button(text="alternative -", font_size=30, size_hint_x=None, size_hint_y=None, width=170, height = 50)
+        self.submit = Button(text="alternative -", font_size=25, size_hint_x=None, size_hint_y=None, width=150, height = 50)
         self.submit.bind(on_press=self.alternativeMinus)
         self.alternative.add_widget(self.submit)
 
@@ -90,6 +90,8 @@ class MyGrid(GridLayout):
         self.userCriteration.add_widget(Label(text="Criteria " + str(self.__criteriaCount),size_hint_x=None, width = 100))
         self.userCriteration.cols = self.__criteriaCount
 
+        self.__updateUserInput(self.__alternativeCount, self.__criteriaCount-1)
+
         #print(self.__criteriaCount)
 
         #print("Name:", name)
@@ -102,15 +104,36 @@ class MyGrid(GridLayout):
         self.userCriteration.cols = self.__criteriaCount
         self.userCriteration.remove_widget(self.userCriteration.children[0])
 
+        self.__updateUserInput(self.__alternativeCount, self.__criteriaCount+1)
+
     def alternativePlus(self, instance):
         self.__alternativeCount += 1
         self.userAlternative.add_widget(Label(text="Alternative " + str(self.__alternativeCount),size_hint_y=None, height = 50))
+
+        self.__updateUserInput(self.__alternativeCount-1, self.__criteriaCount)
     
     def alternativeMinus(self, instance):
         if self.__alternativeCount == 1:
             return
         self.__alternativeCount -= 1
         self.userAlternative.remove_widget(self.userAlternative.children[0])
+
+        self.__updateUserInput(self.__alternativeCount+1, self.__criteriaCount)
+
+    def __updateUserInput(self, oldAltCount, oldCriCount):
+        toAdd = True
+        if oldAltCount * oldCriCount > self.__criteriaCount * self.__alternativeCount :
+            toAdd = False
+        
+        count = abs(oldAltCount * oldCriCount - self.__criteriaCount * self.__alternativeCount)
+
+        for i in range(count):
+            if toAdd:
+                self.userAlternativeInput.add_widget(TextInput(multiline=False,size_hint_y=None, height = 50,size_hint_x=None, width = 100))
+            else:
+                self.userAlternativeInput.remove_widget(self.userAlternativeInput.children[0])
+
+        self.userAlternativeInput.cols = self.__criteriaCount
 
 
 class MyApp(App):
